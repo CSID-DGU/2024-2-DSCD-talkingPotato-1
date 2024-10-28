c5d8cdc055d502a3cba4d365fbc37bc4993382e6
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-10-28 21:27:47 +0900

    fix: Relative Path -> Absolute Path

--- client-app/lib/app/config/app_pages.dart
+++ client-app/lib/app/config/app_pages.dart
@@ -4,8 +4,7 @@ import 'package:wooahan/presentation/view/root/root_screen.dart';
 import 'package:wooahan/presentation/view_model/board/board_binding.dart';
 import 'package:wooahan/presentation/view_model/home/home_binding.dart';
 import 'package:wooahan/presentation/view_model/medication_management/medication_management_binding.dart';
-
-import '../../presentation/view_model/root/root_binding.dart';
+import 'package:wooahan/presentation/view_model/root/root_binding.dart';
 
 abstract class AppPages {
   static List<GetPage> data = [
