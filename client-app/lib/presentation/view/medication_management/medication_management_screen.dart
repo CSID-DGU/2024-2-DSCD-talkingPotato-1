c65f69b6f165959c18038965c877504414e28e02
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-10-29 01:41:14 +0900

    feat: Medication Management Screen UI/UX

--- client-app/lib/presentation/view/medication_management/medication_management_screen.dart
+++ client-app/lib/presentation/view/medication_management/medication_management_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:wooahan/app/config/font_system.dart';
 import 'package:wooahan/core/screen/base_screen.dart';
+import 'package:wooahan/presentation/view/medication_management/widget/schedule_card/schedule_card_list_view.dart';
+import 'package:wooahan/presentation/view/medication_management/widget/schedule_header/schedule_header_view.dart';
+import 'package:wooahan/presentation/view/medication_management/widget/timeline_card/timeline_card_list_view.dart';
 import 'package:wooahan/presentation/view_model/medication_management/medication_management_view_model.dart';
 
 class MedicationManagementScreen
@@ -9,10 +11,17 @@ class MedicationManagementScreen
 
   @override
   Widget buildBody(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Medication ManagementScreen',
-        style: FontSystem.Sub2,
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          TimelineCardListView(),
+          SizedBox(height: 32),
+          ScheduleHeaderView(),
+          SizedBox(height: 32),
+          ScheduleCardListView(),
+          SizedBox(height: 100),
+        ],
       ),
     );
   }
