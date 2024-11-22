a024332dafb191e8be58b37b177e910509b6c82f
tkdwns414 <sangjune2000@naver.com>
2024-11-22 23:15:09 +0900

    feat: Medication Domain

---
+++ server-api/src/main/java/org/dongguk/dscd/wooahan/api/medication/domain/mysql/Schedule.java
@@ -0,0 +1,54 @@
+package org.dongguk.dscd.wooahan.api.medication.domain.mysql;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.dongguk.dscd.wooahan.api.medication.domain.type.ETakenTime;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Table(name = "schedules")
+public class Schedule {
+    /* -------------------------------------------- */
+    /* Default Column ----------------------------- */
+    /* -------------------------------------------- */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    /* -------------------------------------------- */
+    /* Information Column - Input ----------------- */
+    /* -------------------------------------------- */
+    @Column(name = "took_at", nullable = false)
+    private LocalDate tookAt;
+
+    @Column(name = "taken_time", nullable = false)
+    private ETakenTime takenTime;
+
+    /* -------------------------------------------- */
+    /* Relation Column - Parent ------------------- */
+    /* -------------------------------------------- */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medication_id", nullable = false)
+    private Medication medication;
+
+
+    /* -------------------------------------------- */
+    /* Functions ---------------------------------- */
+    /* -------------------------------------------- */
+    public Schedule(
+            LocalDate tookAt,
+            ETakenTime takenTime,
+            Medication medication
+    ) {
+        this.tookAt = tookAt;
+        this.takenTime = takenTime;
+
+        this.medication = medication;
+    }
+
+}
