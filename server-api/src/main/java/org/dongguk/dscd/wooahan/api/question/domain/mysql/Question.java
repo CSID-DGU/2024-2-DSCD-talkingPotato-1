a10b748dc0382fedb97d85982469804ab1fb2f37
tkdwns414 <sangjune2000@naver.com>
2024-11-22 23:26:56 +0900

    feat: Question Domain

---
+++ server-api/src/main/java/org/dongguk/dscd/wooahan/api/question/domain/mysql/Question.java
@@ -0,0 +1,54 @@
+package org.dongguk.dscd.wooahan.api.question.domain.mysql;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.dongguk.dscd.wooahan.api.user.domain.mysql.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Table(name = "questions")
+public class Question {
+    /* -------------------------------------------- */
+    /* Default Column ----------------------------- */
+    /* -------------------------------------------- */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    /* -------------------------------------------- */
+    /* Information Column - Input ----------------- */
+    /* -------------------------------------------- */
+    @Column(name = "content", length = 500, nullable = false)
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    /* -------------------------------------------- */
+    /* Relation Column - Parent ------------------- */
+    /* -------------------------------------------- */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator", nullable = false)
+    private User creator;
+
+    /* -------------------------------------------- */
+    /* Functions ---------------------------------- */
+    /* -------------------------------------------- */
+    @Builder
+    public Question(
+            String content,
+            User creator
+    ) {
+        this.content = content;
+
+        this.createdAt = LocalDateTime.now();
+
+        this.creator = creator;
+    }
+}
