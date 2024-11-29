6bc94bd8a348fa7b1ebc764bbdb5a1636722571e
kurtyoon <akakertcobain@naver.com>
2024-11-29 09:15:58 +0900

    chore: Validation Library

--- client-web/vite.config.ts
+++ client-web/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "@app": path.resolve(__dirname, 'src/app'),
+            "@entities": path.resolve(__dirname, 'src/entities'),
             "@features": path.resolve(__dirname, 'src/features'),
             "@pages": path.resolve(__dirname, 'src/pages'),
             "@shared": path.resolve(__dirname, 'src/shared'),
