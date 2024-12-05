9368745fefad4ad7fe8cb7900bc80e773f918a3f
Sangjune park <74230343+tkdwns414@users.noreply.github.com>
2024-12-05 18:55:27 +0900

    ✨ Feature - Additional APIs (#45)
    * feat: readUserQuestionList API
    
    * feat: readUserMedicationList API

--- server-api/src/main/java/org/dongguk/dscd/wooahan/api/question/controller/query/QuestionQueryV1Controller.java
+++ server-api/src/main/java/org/dongguk/dscd/wooahan/api/question/controller/query/QuestionQueryV1Controller.java
@@ -1,19 +1,25 @@
 package org.dongguk.dscd.wooahan.api.question.controller.query;
 
 import lombok.RequiredArgsConstructor;
+import org.dongguk.dscd.wooahan.api.core.annotation.common.AccountID;
 import org.dongguk.dscd.wooahan.api.core.dto.ResponseDto;
 import org.dongguk.dscd.wooahan.api.question.usecase.ReadQuestionListUseCase;
 import org.dongguk.dscd.wooahan.api.question.usecase.ReadQuestionUseCase;
+import org.dongguk.dscd.wooahan.api.question.usecase.ReadUserQuestionListUseCase;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/questions")
+@RequestMapping("/v1")
 public class QuestionQueryV1Controller {
 
     private final ReadQuestionListUseCase readQuestionListUseCase;
     private final ReadQuestionUseCase readQuestionUseCase;
+    private final ReadUserQuestionListUseCase readUserQuestionUseCase;
 
     /**
      * 9-1. 질문 목록 조회
@@ -21,7 +27,7 @@ public class QuestionQueryV1Controller {
      * @param query 쿼리
      * @return 응답 DTO
      */
-    @GetMapping
+    @GetMapping("/questions")
     public ResponseDto<?> readQuestionList(
             @RequestParam(required = false, name = "q") String query,
             Pageable pageable
@@ -35,8 +41,21 @@ public class QuestionQueryV1Controller {
      * @param questionId 질문 ID
      * @return 응답 DTO
      */
-    @GetMapping("/{questionId}")
+    @GetMapping("/questions/{questionId}")
     public ResponseDto<?> readQuestion(@PathVariable Long questionId) {
         return ResponseDto.ok(readQuestionUseCase.execute(questionId));
     }
+
+    /**
+     * 9-5. 본인 질문 목록 조회
+     * @param accountId 계정 ID
+     * @return 응답 DTO
+     */
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/users/questions")
+    public ResponseDto<?> readUserQuestionList(@AccountID UUID accountId) {
+        return ResponseDto.ok(readUserQuestionUseCase.execute(accountId));
+    }
+
+
 }
--- server-api/src/main/java/org/dongguk/dscd/wooahan/api/question/repository/mysql/custom/QuestionRepositoryCustomImpl.java
+++ server-api/src/main/java/org/dongguk/dscd/wooahan/api/question/repository/mysql/custom/QuestionRepositoryCustomImpl.java
@@ -9,11 +9,13 @@ import lombok.RequiredArgsConstructor;
 import org.dongguk.dscd.wooahan.api.question.domain.mysql.QAnswer;
 import org.dongguk.dscd.wooahan.api.question.domain.mysql.QQuestion;
 import org.dongguk.dscd.wooahan.api.question.dto.projection.ReadQuestionListProjection;
+import org.dongguk.dscd.wooahan.api.question.dto.projection.ReadUserQuestionListProjection;
 import org.dongguk.dscd.wooahan.api.user.domain.mysql.QUser;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -52,6 +54,55 @@ public class QuestionRepositoryCustomImpl implements QuestionRepositoryCustom {
                                                 Expressions.numberTemplate(
                                                     Long.class,
                                                     "SUM(CASE WHEN {0} IS NULL THEN 1 ELSE 0 END)", answer.creator
+                                                ).eq(answer.id.count())
+                                        ).then("AI")
+                                        .otherwise("EXPERT")
+                                        .as("answerStatus"),
+                                question.createdAt,
+                                Expressions.as(
+                                        Expressions.cases()
+                                                .when(answer.id.countDistinct().isNull()).then(0L)
+                                                .otherwise(answer.id.countDistinct()),
+                                        "answerCount"
+                                ),
+                                question.creator.nickname,
+                                question.creator.id
+                        )
+                )
+                .from(question)
+                .leftJoin(question.creator, user)
+                .leftJoin(question.answers, answer)
+                .where(question.id.in(questionIdList))
+                .groupBy(question.id)
+                .orderBy(question.createdAt.desc())
+                .fetch();
+    }
+
+    @Override
+    public List<ReadUserQuestionListProjection> findUserQuestionList(UUID accountId) {
+        QQuestion question = QQuestion.question;
+        QAnswer answer = QAnswer.answer;
+        QUser user = QUser.user;
+
+        List<Long> questionIdList = queryFactory
+                .select(question.id)
+                .from(question)
+                .where(question.creator.id.eq(accountId))
+                .orderBy(question.createdAt.desc())
+                .limit(3L)
+                .fetch();
+
+        return queryFactory.select(
+                        Projections.constructor(
+                                ReadUserQuestionListProjection.class,
+                                question.id.as("id"),
+                                question.content.substring(0, 50).as("preview"),
+                                Expressions.cases()
+                                        .when(answer.id.count().lt(1L)).then("NONE")
+                                        .when(
+                                                Expressions.numberTemplate(
+                                                        Long.class,
+                                                        "SUM(CASE WHEN {0} IS NULL THEN 1 ELSE 0 END)", answer.creator
                                                 ).eq(answer.id.count())
                                         ).then("AI")
                                         .otherwise("EXPERT")
