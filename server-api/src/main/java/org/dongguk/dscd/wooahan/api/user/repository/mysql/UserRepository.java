7eb5a28029bdcaf1165903250502862e41ee9148
Sangjune park <74230343+tkdwns414@users.noreply.github.com>
2024-12-07 01:47:01 +0900

    ✨ Feature - FCM Notification (#57)
    * feat: NotificationService with FCM
    
    * feat: sendNotification API for presentation

--- server-api/src/main/java/org/dongguk/dscd/wooahan/api/user/repository/mysql/UserRepository.java
+++ server-api/src/main/java/org/dongguk/dscd/wooahan/api/user/repository/mysql/UserRepository.java
@@ -4,8 +4,26 @@ import org.dongguk.dscd.wooahan.api.user.domain.mysql.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    List<User> findAllByBreakfastTimeAndIsAllowedNotificationAndDeviceTokenIsNotNull(
+            LocalTime nowTime,
+            Boolean isAllowedServiceNotification
+    );
+
+    List<User> findAllByLunchTimeAndIsAllowedNotificationAndDeviceTokenIsNotNull(
+            LocalTime nowTime,
+            Boolean isAllowedServiceNotification
+    );
+
+    List<User> findAllByDinnerTimeAndIsAllowedNotificationAndDeviceTokenIsNotNull(
+            LocalTime nowTime,
+            Boolean isAllowedServiceNotification
+    );
+
 }
