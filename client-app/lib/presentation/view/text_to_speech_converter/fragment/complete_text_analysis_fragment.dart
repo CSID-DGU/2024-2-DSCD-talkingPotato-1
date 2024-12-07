a7801da3bccf69b4c3f5b773e1edd508ca521476
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-12-07 18:37:01 +0900

    feat: TTS Modeul

--- client-app/lib/presentation/view/text_to_speech_converter/fragment/complete_text_analysis_fragment.dart
+++ client-app/lib/presentation/view/text_to_speech_converter/fragment/complete_text_analysis_fragment.dart
@@ -5,9 +5,9 @@ import 'package:get/get.dart';
 import 'package:wooahan/app/config/color_system.dart';
 import 'package:wooahan/app/config/font_system.dart';
 import 'package:wooahan/core/screen/base_screen.dart';
+import 'package:wooahan/presentation/view/text_to_speech_converter/widget/tts_button/tts_button.dart';
 import 'package:wooahan/presentation/view_model/text_to_speech_converter/text_to_speech_converter_view_model.dart';
 import 'package:wooahan/presentation/widget/common/button/primary/primary_fill_button.dart';
-import 'package:wooahan/presentation/widget/common/image/svg_image_view.dart';
 
 class CompleteTextAnalysisFragment
     extends BaseScreen<TextToSpeechConverterViewModel> {
@@ -21,96 +21,74 @@ class CompleteTextAnalysisFragment
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 32),
-          const Text(
-            '사진 분석이 완료되었어요',
-            style: FontSystem.H2,
-          ),
-          Text(
-            '입술 모양을 눌러 들어보세요',
-            style: FontSystem.Sub2.copyWith(
-              color: ColorSystem.neutral.shade700,
-            ),
-          ),
+          ..._buildDescriptionViews(),
           const Spacer(),
-          GestureDetector(
-            onTap: viewModel.updateViewing,
-            child: Obx(() {
-              if (viewModel.isViewingImage) {
-                return Image.file(
-                  File(viewModel.image!.path),
-                  width: Get.width,
-                  height: Get.height * 0.45,
-                  fit: BoxFit.cover,
-                );
-              }
-
-              return Container(
-                width: Get.width,
-                height: Get.height * 0.45,
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: ColorSystem.neutral.shade100,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: SingleChildScrollView(
-                  child: Text(
-                    viewModel.analysisResult,
-                    style: FontSystem.Sub3,
-                  ),
-                ),
-              );
-            }),
-          ),
+          _buildAnalysisResultView(),
+          const SizedBox(height: 32),
+          const TtsButton(),
           const Spacer(),
-          Center(
-            child: Obx(
-              () => FilledButton(
-                onPressed: viewModel.updateListening,
-                style: FilledButton.styleFrom(
-                  // Padding
-                  padding: EdgeInsets.zero,
-
-                  // Size
-                  minimumSize: const Size(64, 64),
-                  fixedSize: const Size(64, 64),
-
-                  elevation: 1,
-
-                  // Color
-                  backgroundColor: ColorSystem.secondary,
-                  foregroundColor: ColorSystem.white,
-
-                  disabledBackgroundColor: ColorSystem.neutral.shade300,
-
-                  // Border
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(32),
-                    ),
-                  ),
-                ),
-                child: viewModel.isListening
-                    ? const Icon(
-                        Icons.stop,
-                        size: 40,
-                      )
-                    : const SvgImageView(
-                        assetPath: 'assets/icons/lips.svg',
-                        width: 40,
-                      ),
-              ),
-            ),
-          ),
-          const Spacer(),
-          PrimaryFillButton(
-            width: Get.width,
-            height: 64,
-            content: '완료',
-            onPressed: Get.back,
-          ),
+          _buildCompleteButton(),
           SizedBox(height: GetPlatform.isAndroid ? 20 : 40),
         ],
       ),
     );
+  }
+
+  List<Widget> _buildDescriptionViews() {
+    return [
+      const Text(
+        '사진 분석이 완료되었어요',
+        style: FontSystem.H2,
+      ),
+      Text(
+        '버튼을 눌러 들어보세요',
+        style: FontSystem.Sub2.copyWith(
+          color: ColorSystem.neutral.shade700,
+        ),
+      ),
+    ];
+  }
+
+  Widget _buildAnalysisResultView() {
+    return GestureDetector(
+      onTap: viewModel.updateViewing,
+      child: Obx(() {
+        if (viewModel.isViewingImage) {
+          return Image.file(
+            File(viewModel.image!.path),
+            width: Get.width,
+            height: Get.height * 0.4,
+            fit: BoxFit.cover,
+          );
+        }
+
+        return Container(
+          width: Get.width,
+          height: Get.height * 0.4,
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: ColorSystem.neutral.shade100,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: SingleChildScrollView(
+            child: Text(
+              viewModel.analysisResult,
+              style: FontSystem.Sub3,
+            ),
+          ),
+        );
+      }),
+    );
+  }
+
+  Widget _buildCompleteButton() {
+    return Obx(() {
+      return PrimaryFillButton(
+        width: Get.width,
+        height: 64,
+        content: viewModel.isSpeaking ? '음성으로 듣고 있어요' : '돌아가기',
+        onPressed: viewModel.isSpeaking ? null : Get.back,
+      );
+    });
   }
 }
